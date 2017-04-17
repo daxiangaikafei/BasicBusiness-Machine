7dbf0c3a839603fe99b01e6d7588502a0e80ad4c
FIRST_NAME LAST_NAME <mail@xuechen.wang>
2017-04-17 16:34:51 +0800

    update dialogTag.vue for json data

--- src/components/dialogTag.vue
+++ src/components/dialogTag.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="tag-container">
-		<div class="tag-item" v-on:click="invokeSendMsg(tag.value,tag.tagDetailId)" v-for="tag in tagList.items">
-			{{ tag.value }}
+		<div class="tag-items" v-for="itemList in tagList">
+			<div class="tag-item" v-on:click="invokeSendMsg(tag.value,tag.tagDetailId)" v-for="tag in itemList.items">
+				{{ tag.value }}
+			</div>
 		</div>
+		
 	</div>
 </template>
 <script>
@@ -23,15 +26,22 @@
 		width: 100%;
 		overflow-y: auto;
 		white-space: nowrap;
-		.tag-item{
-			position: relative;
+		.tag-items{
 			display: inline-block;
-			border-radius: 20px 20px;
-			height: 30px;
-			line-height: 30px;
-			border: solid 1px #d5d5d5;
-			margin: 0 10px;
-			padding: 0 8px;
+			.tag-item{
+				position: relative;
+				display: inline-block;
+				border-radius: 20px 20px;
+				height: 30px;
+				line-height: 30px;
+				border: solid 1px #d5d5d5;
+				margin: 0 10px;
+				padding: 0 8px;
+				max-width: 100px;
+				overflow: hidden;
+				text-overflow: ellipsis;
+			}
 		}
+		
 	}
 </style>
