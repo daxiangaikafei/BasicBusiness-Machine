83c3d73e983e276e23c17c0eaad9e68009a6b627
FIRST_NAME LAST_NAME <mail@xuechen.wang>
2017-04-20 16:10:38 +0800

    update receiveMsgCoustomService.vue to fix bug

--- src/components/receiveMsgCustomService.vue
+++ src/components/receiveMsgCustomService.vue
@@ -5,7 +5,7 @@
 		<div class="receiveMsg-custom-service" v-if="item.receiveObj.questionType == 'customService'">
 			<img class="img-icon" src="../static/images/receiveMsg-icon.png">
 			<div class="receiveMsg-context-text">亲，小智不明白您的问题噢，您可以联系客服。
-				<a v-if="item.receiveObj.data.link != undefined && item.receiveObj.data.link != ''" class="customer-service" target="_blank" href="newtab://ai.qbao.com/service">人工客服</a>
+				<a class="customer-service" target="_blank" href="newtab://ai.qbao.com/service">人工客服</a>
 			</div>
 		</div>
 	</transition>
