569d3df8e5b430536af142258514561e0d8d410a
FIRST_NAME LAST_NAME <mail@xuechen.wang>
2017-05-04 11:41:46 +0800

    update dialogContainer.vue

--- src/components/dialogContainer.vue
+++ src/components/dialogContainer.vue
@@ -1,23 +1,23 @@
 <template>
-	<div id="dialog-list">
-		<div id="dialog-container" @click="hideReminder">
-			<div class="dialog-item" v-for="item in itemList">
-				<sendMsg v-bind:item="item" v-if="item.type == 1"></sendMsg>
-				<div class="receiveMsg-container" v-if="item.type == 2">
-					<receiveMsgContext v-bind:item="item"></receiveMsgContext>
-					<receiveMsgGoods v-bind:item="item"></receiveMsgGoods>
-                    			<receiveMsgNews v-bind:item="item"></receiveMsgNews>
-					<receiveMsgWeather v-bind:item="item"></receiveMsgWeather>
-					<receiveMsgQuestion v-on:getAnswer="getAnswer" v-bind:item="item"></receiveMsgQuestion>
-					<receiveMsgInit v-if="initFlag == 0" v-bind:item="item"></receiveMsgInit>
-					<receiveMsgCustomService v-bind:item="item"></receiveMsgCustomService>
-				</div>
-			</div>
-		</div>
-		<dialogTag v-bind:tagList="tagList" v-on:sendTagMsg="sendTagMsg" v-if="id != 0"></dialogTag>
+    <div id="dialog-list">
+        <div id="dialog-container" @click="hideReminder">
+            <div class="dialog-item" v-for="item in itemList">
+                <sendMsg v-bind:item="item" v-if="item.type == 1"></sendMsg>
+                <div class="receiveMsg-container" v-if="item.type == 2">
+                    <receiveMsgContext v-bind:item="item"></receiveMsgContext>
+                    <receiveMsgGoods v-bind:item="item"></receiveMsgGoods>
+                                <receiveMsgNews v-bind:item="item"></receiveMsgNews>
+                    <receiveMsgWeather v-bind:item="item"></receiveMsgWeather>
+                    <receiveMsgQuestion v-on:getAnswer="getAnswer" v-bind:item="item"></receiveMsgQuestion>
+                    <receiveMsgInit v-if="initFlag == 0" v-bind:item="item"></receiveMsgInit>
+                    <receiveMsgCustomService v-bind:item="item"></receiveMsgCustomService>
+                </div>
+            </div>
+        </div>
+        <dialogTag v-bind:tagList="tagList" v-on:sendTagMsg="sendTagMsg" v-if="id != 0"></dialogTag>
         <reminder v-if="isShowReminder"></reminder>
-		<inputArea @hideReminder='hideReminder' @sendTagMsg='sendTagMsg' @sendMsg='sendMsg' v-bind="{parseId: id}"></inputArea>
-	</div>
+        <inputArea @hideReminder='hideReminder' @sendTagMsg='sendTagMsg' @sendMsg='sendMsg' v-bind="{parseId: id}"></inputArea>
+    </div>
 </template>
 
 <script>
@@ -35,7 +35,7 @@
     import dialogTag from './dialogTag'
     import reminder from './reminder'
     import ApiControl from '../config/envConfig.home'
-    var env = 'debug';// set env type for debug or product
+    var env = 'product';// set env type for debug or product
     // var dataArray = ["question1","question2","question3","question4","question5"]
     export default {
         name: 'dialogContainer',
@@ -60,7 +60,7 @@
                 initFlag: 1,
                 tagList: {},
                 device: '',
-                isShowReminder: true
+                isShowReminder: false
             }
         },
         components: {
@@ -105,16 +105,15 @@
             },
             // normal send message for user input
             sendMsg: function(value, id) {
-                // sendMsgByApp("invoke send msg by app method");
                 this.beforeSend(value);
                 // this.isShowReminder = false;
 
                 // for server
                 ajax('GET', ApiControl.getApi(env,"question"),{
-                	lat: this.lat,
-                	lon: this.lng,
-                	device: this.device,
-                	q: id == '' ? value: id
+                    lat: this.lat,
+                    lon: this.lng,
+                    device: this.device,
+                    q: id == '' ? value: id
                 }).
                 then(res => {
                     this.receiveMsg(res)
@@ -187,21 +186,18 @@
 
             this.device = utils.getDevice();
 
-            window.sendMsgByApp = function(test){
-                console.log(test);
-            }
         },
         mounted() {
+            const that = this;
             document.getElementById('dialog-container').style.height = (document.body.clientHeight - 100) + 'px'
-
             // not index and goods page, init to get tag item list
             if(this.id != 0 && this.id != 1035){
-            	ajax('GET', ApiControl.getApi(env,"tagItem"),{
-            		id: this.menuId
-            	}).
-            	then(res => {
-            	    this.tagList = res.data
-            	})
+                ajax('GET', ApiControl.getApi(env,"tagItem"),{
+                    id: this.menuId
+                }).
+                then(res => {
+                    this.tagList = res.data
+                })
 
                 ajax('GET', ApiControl.getApi(env,"details"),{
                     lat: this.lat,
@@ -226,6 +222,14 @@
              })
             }
 
+            window.sendMsgByApp = window.sendMsgByApp || function(data){
+                if(!data || data == ""){
+                    that.isShowReminder = true;
+                }else{
+                    that.sendMsg(data);
+                }
+            }
+
         },
         computed() {}
     }
@@ -246,52 +250,6 @@
         width: 100%;
         zoom: 1;
         overflow: hidden;
-        // .sendMsg-container {
-        //     width: 100%;
-        //     text-align: right;
-        //     margin-top: 30px;
-        //     .sendMsg-text {
-        //         display: inline-block;
-        //         position: relative;
-        //         min-height: 50px;
-        //         max-height: 135px;
-        //         line-height: 20px;
-        //         border: solid 1px #fff;
-        //         background: #fff;
-        //         border-radius: 10px;
-        //         vertical-align: top;
-        //         max-width: 250px;
-        //         margin-right: 20px;
-        //         padding: 15px 17.5px 15px 17.5px;
-        //         overflow: hidden;
-        //         text-overflow: ellipsis;
-        //     }
-        //     .sendMsg-text:before {
-        //         content: '';
-        //         width: 0;
-        //         height: 0;
-        //         border: 10px solid transparent;
-        //         border-left-color: #fff;
-        //         position: absolute;
-        //         left: 100%;
-        //         top: 40%;
-        //         margin-top: -10px;
-        //     }
-        //     .sendMsg-text:after {
-        //         content: "";
-        //         width: 0;
-        //         height: 0;
-        //         border: 8px solid transparent;
-        //         border-left-color: #FFF;
-        //         position: absolute;
-        //         left: 100%;
-        //         top: 40%;
-        //         margin-top: -8px;
-        //     }
-        //     img {
-        //         margin-right: 12px;
-        //     }
-        // }
         .receiveMsg-container {
             width: 100%;
             text-align: right;
